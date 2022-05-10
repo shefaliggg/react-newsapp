85740306c384ea045e539c45380b9a55d7aad6a8
Shefali G <shefali@traderies.com>
2022-05-10 19:41:28 +0530

    #38

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -6,8 +6,11 @@ export class Newsitem extends Component {
     return (
       <>
         <div className="card my-3" style={{ width: "18rem" }}>
-          <span class="position-absolute top-0 translate-middle badge rounded-pill 
-          bg-danger" style={{zIndex:10, left:'85%'}}>{source}</span>
+
+        <div style={{display:"flex", justifyContent:"flex-end",position:"absolute",right:"0"}}>
+          <span class=" badge rounded-pill bg-danger">{source}</span>
+        </div>
+
           <img src={img} className="card-img-top" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
